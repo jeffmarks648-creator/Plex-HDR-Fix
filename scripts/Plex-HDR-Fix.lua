e78397a2ed9ec2308c8ec0e44596bc25f95ff90f
jeffmarks648 <jeffmarks648@gmail.com>
2026-03-08 02:26:14 +0800

    Change initial hwdec mode to auto and use lua to re-init filters for auto-copy.

--- scripts/Plex-HDR-Fix.lua
+++ scripts/Plex-HDR-Fix.lua
@@ -1,12 +1,10 @@
 mp.observe_property("hwdec", "string", function(name, val)
-    if val == "auto-copy" then
-     
+    if val == "auto-copy" then     
         local vf = mp.get_property("vf")
         if vf ~= "" then
             mp.set_property("vf", "")
             mp.set_property("vf", vf)
             mp.osd_message("VF Reloaded for auto-copy")
         end
-
     end
 end)
